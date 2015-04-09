0e10974c4428c596e90b87cfbb52dbfb1b3f769f
cm99521 <cm99521@mcc.com>
2015-04-09 20:24:15 +0800

    update

--- src/main/java/com/viti/activiti/Book.java
+++ src/main/java/com/viti/activiti/Book.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.viti.activiti;
 
 import java.util.ArrayList;
@@ -66,72 +65,3 @@ public class Book {
 		this.isbn = isbn;
 	}
 }
-=======
-package com.viti.activiti;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
-@Entity
-public class Book {
-
-	@Id
-	@GeneratedValue
-	private int id;
-
-	private String title;
-
-	private String subTitle;
-
-	private String isbn;
-
-	@ElementCollection(fetch = FetchType.EAGER)
-	private List<String> authors = new ArrayList<String>();
-
-	public List<String> getAuthors() {
-		return authors;
-	}
-
-	public void setAuthors(List<String> authors) {
-		this.authors = authors;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getSubTitle() {
-		return subTitle;
-	}
-
-	public void setSubTitle(String subTitle) {
-		this.subTitle = subTitle;
-	}
-
-	public String getIsbn() {
-		return isbn;
-	}
-
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
-	}
-}
->>>>>>> refs/remotes/origin/master
--- src/main/java/com/viti/activiti/BookService.java
+++ src/main/java/com/viti/activiti/BookService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.viti.activiti;
 
 import java.util.List;
@@ -23,29 +22,3 @@ public class BookService {
 		return book;
 	}
 }
-=======
-package com.viti.activiti;
-
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-
-public class BookService {
-
-	@PersistenceContext
-	private EntityManager entityManager;
-	
-	public Book createBook(List<String> authorList){
-		Book book = new Book();
-		for(String author : authorList){
-			book.getAuthors().add(author);
-		}
-		
-		entityManager.persist(book);
-		System.out.println("leaving createbook@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-		return book;
-	}
-}
->>>>>>> refs/remotes/origin/master
